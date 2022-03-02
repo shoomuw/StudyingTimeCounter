178df5bada48ff23ee54653dba7888231e82380e
SWD-002\falma <falmars.g@gmail.com>
2022-03-03 01:06:04 +0900

    first commit

---
+++ GUI.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class GUI extends Frame implements Runnable{
+    /** スレッド */
+    private Thread time;
+
+    /**
+     * コンストラクタ
+     * @param height 高さ
+     * @param width 幅
+     */
+    public GUI(int height, int width){
+
+    }
+}
