a243d0e328c66d73bd5989310d81db48fbfd7782
SWD-002\falma <falmars.g@gmail.com>
2022-03-04 18:50:13 +0900

    paintメソッド、runメソッド、その他フィールドの追加

--- StopWatch.java
+++ StopWatch.java
@@ -5,13 +5,31 @@ import java.util.*;
 
 public class StopWatch extends GUI implements Runnable{
 
-    private Thread thread;
+    private Thread thread = null;
+
+    private Calendar date;
+
+    private int s = 0;
+    private int m = 0;
+    private int h = 0;
 
     public StopWatch(int width, int height){
         setSize(width, height);
     }
 
+    public void paint(Graphics g){
+        
+    }
+    
     public void run(){
+        while(Thread.currentThread()==thread){
+            repaint();
+            try{
+                thread.sleep(1000);
+            }
+            catch(InterruptedException e){
 
+            }
+        }
     }
 }
